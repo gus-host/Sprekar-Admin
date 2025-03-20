25cbafc96f046ef2126b0ea669bff86d366b00d3
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2025-03-20 16:53:59 +0100

    deploying to aws

--- src/app/resetPassword/page.tsx
+++ src/app/resetPassword/page.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
-import AuthLayout from '../_partials/_layout/AuthLayout';
-import TextInput from '../_partials/TextInputs';
-import Button from '@/components/Button';
-import ResetPassword from './_partials/ResetPassword';
+// import Link from 'next/link';
+import AuthLayout from "../_partials/_layout/AuthLayout";
+// import TextInput from '../_partials/TextInputs';
+// import Button from '@/components/Button';
+import ResetPassword from "./_partials/ResetPassword";
 
 export const metadata = {
-  title: 'Reset Password',
+  title: "Reset Password",
 };
 
 export default function page() {
