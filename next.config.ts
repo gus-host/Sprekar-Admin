0114294afec332b03f5fdff3bccebf7ccc849dfe
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2025-05-17 11:13:40 +0100

    deployment fixes

--- next.config.ts
+++ next.config.ts
@@ -13,11 +13,15 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     PRICEID: process.env.PRICEID,
   },
-};
-
-module.exports = {
   images: {
-    remotePatterns: [new URL("https://lh3.googleusercontent.com/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
