aee274846931eea66b0b5ccad2e0c1329ba97c89
cipher <fynhacker@gmail.com>
2025-08-04 17:32:00 +0100

    added i18n to contact-us page

--- src/app/[lng]/contact-us/contact-us.validation.ts
+++ src/app/[lng]/contact-us/contact-us.validation.ts
@@ -1,8 +1,18 @@
 import * as Yup from "yup";
 
-export const validationSchema = Yup.object().shape({
-  firstName: Yup.string().required("First name is required"),
-  lastName: Yup.string().required("Last name is required"),
-  email: Yup.string().email("Invalid email").required("Email is required"),
-  message: Yup.string().required("Message` is required"),
-});
+export function getValidationSchema(t: (key: string) => string) {
+  return Yup.object().shape({
+    firstName: Yup.string().required(
+      t("contact.form.validation.firstName.required")
+    ),
+    lastName: Yup.string().required(
+      t("contact.form.validation.lastName.required")
+    ),
+    email: Yup.string()
+      .email(t("contact.form.validation.email.invalid"))
+      .required(t("contact.form.validation.email.required")),
+    message: Yup.string().required(
+      t("contact.form.validation.message.required")
+    ),
+  });
+}
--- src/app/[lng]/contact-us/page.tsx
+++ src/app/[lng]/contact-us/page.tsx
@@ -7,14 +7,19 @@ import Header from "@/app/[lng]/_partials/Header";
 import HomePageLayout from "@/app/[lng]/_partials/_layout/HomePageLayout";
 
 import bgImg from "@/../public/bgImg.png";
-import { validationSchema } from "./contact-us.validation";
 import { FormField } from "@/app/[lng]/_partials/FormField";
 import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import H1 from "@/app/[lng]/_partials/H1";
+import { useParams } from "next/navigation";
+import { useTranslation } from "@/app/i18n/client";
+import { getValidationSchema } from "./contact-us.validation";
 
 export default function Page() {
+  const { lng } = useParams<{ lng: string }>();
+  const { t } = useTranslation(lng, "contact");
+  const validationSchema = getValidationSchema(t);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const formik = useFormik({
     validationSchema: validationSchema,
@@ -34,14 +39,18 @@ export default function Page() {
         console.log(response);
         if (response.status === 201 || response.status === 200) {
           console.log(response.data);
-          toast.success("Message successfully sent!");
+          toast.success(`${t("contact.toast.success")}`);
         } else {
           console.log(response.data);
-          toast.error(response.data.data.message || "An error occured");
+          toast.error(
+            response.data.data.message || `${t("contact.toast.error")}`
+          );
         }
       } catch (error) {
         if (axios.isAxiosError(error))
-          toast.error(error?.response?.data?.message || "An error occured.");
+          toast.error(
+            error?.response?.data?.message || `${t("contact.toast.error")}`
+          );
         if (error instanceof Error) console.log(error);
       } finally {
         setIsSubmitting(false);
@@ -50,15 +59,15 @@ export default function Page() {
   });
   return (
     <div className="bg-[#F8F8F8]">
-      <HomePageLayout>
+      <HomePageLayout lng={lng}>
         <div style={{ overflowX: "hidden" }}>
           <section className="relative max-[750px]:pt-[80px] min-h-[1051px]">
             <div className="relative z-10">
-              <Header />
+              <Header lng={lng} />
               <div className="mx-auto px-[20px]">
-                <H1>We’d love to help</H1>
+                <H1>{t("contact.title")}</H1>
                 <p className="text-[#323232B2] text-[16px] text-center max-w-[520px] mx-auto mb-[60px]">
-                  Reach out and we’ll get back to you within 24 hours!
+                  {t("contact.subtitle")}
                 </p>
               </div>
               <div className="px-[30px]">
@@ -69,8 +78,10 @@ export default function Page() {
                   <div className="flex flex-col md:flex-row gap-x-3 gap-y-6 mb-6">
                     <FormField
                       as="input"
-                      label="First name"
-                      placeholder="Enter your name"
+                      label={`${t("contact.form.fields.firstName.label")}`}
+                      placeholder={`${t(
+                        "contact.form.fields.firstName.placeholder"
+                      )}`}
                       name="firstName"
                       value={formik.values.firstName}
                       onChange={formik.handleChange}
@@ -79,8 +90,10 @@ export default function Page() {
                     />
                     <FormField
                       as="input"
-                      label="Last name"
-                      placeholder="Enter your name"
+                      label={`${t("contact.form.fields.lastName.label")}`}
+                      placeholder={`${t(
+                        "contact.form.fields.lastName.placeholder"
+                      )}`}
                       name="lastName"
                       value={formik.values.lastName}
                       onChange={formik.handleChange}
@@ -91,8 +104,10 @@ export default function Page() {
                   <div className="mb-6">
                     <FormField
                       as="input"
-                      label="Email Address"
-                      placeholder="Enter your email address"
+                      label={`${t("contact.form.fields.email.label")}`}
+                      placeholder={`${t(
+                        "contact.form.fields.email.placeholder"
+                      )}`}
                       name="email"
                       value={formik.values.email}
                       onChange={formik.handleChange}
@@ -104,8 +119,10 @@ export default function Page() {
                   <div className="mb-[46px]">
                     <FormField
                       as="textarea"
-                      label="Message"
-                      placeholder="Enter your message"
+                      label={`${t("contact.form.fields.message.label")}`}
+                      placeholder={`${t(
+                        "contact.form.fields.message.placeholder"
+                      )}`}
                       name="message"
                       value={formik.values.message}
                       onChange={formik.handleChange}
@@ -127,7 +144,9 @@ export default function Page() {
                     }}
                     disabled={isSubmitting}
                   >
-                    {isSubmitting ? "Sending Message..." : "Send message"}
+                    {isSubmitting
+                      ? `${t("contact.form.button.loading")}`
+                      : `${t("contact.form.button.default")}`}
                   </button>
                 </form>
               </div>
